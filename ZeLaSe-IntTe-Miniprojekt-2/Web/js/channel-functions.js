7e44025061c597639c0136bdd222b47ecd6d8680
Cyrill Lam <cyrill.lam@gmail.com>
2012-11-19 14:05:04 +0100

    UI for channelcreationscreen

--- ZeLaSe-IntTe-Miniprojekt-2/Web/js/channel-functions.js
+++ ZeLaSe-IntTe-Miniprojekt-2/Web/js/channel-functions.js
@@ -1,3 +1,16 @@
 $(document).delegate("#channel", "pagecreate", function () {
-    
+    loadChannels();
+    var token;
+    var channelName;
+    function loadChannels() {
+        //setTimeout(loadChannels, 5000);
+        $.ajax({
+            type: "POST",
+            url: serverUrl + "playerToken="+token+"&channelName="+channelName,
+            dataType: "xml",
+            success: function (xml) {
+                
+            }
+        });
+    }
 });
