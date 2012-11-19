158d9aa8c0a9317b25f21cf88c31f3b24d86684e
Patrick Zenh�usern <pz@tennhouse.ch>
2012-11-19 09:18:05 +0100

    add load channel function

---
+++ ZeLaSe-IntTe-Miniprojekt-2/Web/js/global-functions.js
@@ -0,0 +1,7 @@
+//var serverUrl = 'http://sifsv-80018.hsr.ch/Service/ChatService.asmx/';
+var serverUrl = './Service/ChatService.asmx/';
+
+(function($) {
+    $.support.cors = true;
+
+})(jQuery);
--- ZeLaSe-IntTe-Miniprojekt-2/Web/js/lobby-functions.js
+++ ZeLaSe-IntTe-Miniprojekt-2/Web/js/lobby-functions.js
@@ -1,3 +1,25 @@
 $(document).delegate("#lobby", "pagecreate", function () {
+    loadChannels();
     
+    function loadChannels() {
+        setTimeout(loadChannels, 5000);
+        $.ajax({
+            type: "POST",
+            url: serverUrl + "GetChats ",
+            dataType: "xml",
+            success: function (xml) {
+                var channels = $("#availableChannels");
+                channels.html("");
+                $(xml).find("Chat").each(function () {
+                    var chatName = $(this).find('Name').text();
+                    var chatId = $(this).find('Id').text();
+                    var numberOfPlayers = $(this).find('Players').find('Player').size();
+                    channels.append('<p><a href="#chat" id="' + chatId + '" data-role="button">' + chatName + ' (' + numberOfPlayers + ')</a></p>');
+                });
+                channels.trigger("create");
+            }
+        });
+    }
 });
+
+
