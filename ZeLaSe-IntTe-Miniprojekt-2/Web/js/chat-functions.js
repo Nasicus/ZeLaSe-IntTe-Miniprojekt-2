b489e7b994d466f4601901e6d9bed14baeb19aa8
Cyrill Lam <cyrill.lam@gmail.com>
2012-11-23 13:36:30 +0100

    join chat

--- ZeLaSe-IntTe-Miniprojekt-2/Web/js/chat-functions.js
+++ ZeLaSe-IntTe-Miniprojekt-2/Web/js/chat-functions.js
@@ -1,6 +1,62 @@
 $(document).delegate("#chat", "pageinit", function () {
 
+
+    var playerToken;
+    var username;
+    var channel;
     if ($.mobile.pageData && $.mobile.pageData.username) {
-        console.log("Parameter username=" + $.mobile.pageData.username);
-    }    
+        username = $.mobile.pageData.username;
+       
+    } else {
+        //window.location.href = "http://google.com"
+    }
+    if ($.mobile.pageData && $.mobile.pageData.playerToken) {
+        playerToken = $.mobile.pageData.playerToken;
+
+    } if ($.mobile.pageData && $.mobile.pageData.channel) {
+        channel = $.mobile.pageData.channel;
+    }
+
+    joinChat();
+
+    setUserlabel(username);
+    setChatroomlabel(channel);
+
+    
+    
+
+
+    function setUserlabel() {
+        $("#userchatnamelabel").text("Enter your message(" + username + " says)");
+    }
+
+    function setChatroomlabel(channel) {
+        if ($.mobile.pageData && $.mobile.pageData.channel) {
+            $.ajax({
+                type: "POST",
+                url: serverUrl + "GetChat",
+                data: { chatId: channel },
+                dataType: "xml",
+                async: false,
+                success: function (xml) {
+                    $("#chatnamelabel").text($(xml).find("Name").text());
+                }
+            });
+        }
+    }
+
+    function joinChat() {
+        $.ajax({
+            type: "POST",
+            url: serverUrl + "joinChat",
+            data: {
+                playerToken: playerToken,
+                chatId: channel,
+                userName: username
+            },
+            dataType: "xml",
+            async: false,
+            success: function (xml) {}
+        });
+    }
 });
