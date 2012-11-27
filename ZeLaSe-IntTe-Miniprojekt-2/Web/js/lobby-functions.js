b4a01738b6e9835ff83737e6c77414a36f54cc29
fsenn <fsenn@hsr.ch>
2012-11-27 16:00:20 +0100

    Login statt connect

--- ZeLaSe-IntTe-Miniprojekt-2/Web/js/lobby-functions.js
+++ ZeLaSe-IntTe-Miniprojekt-2/Web/js/lobby-functions.js
@@ -1,85 +1,73 @@
 $(document).delegate("#lobby", "pagecreate", function () {
-    var errorField = $("#errorLobby");
-    var playerToken = "";
-    var shouldPollForNewChatRooms = true;
-    
     $(document).delegate("#lobby", "pagebeforehide", pageBeforeHideFunction);
     $(document).delegate("#lobby", "pagebeforeshow", pageBeforeShowFunction);
+
+    var errorField = $("#errorLobby");
+    var playerToken = "";
+    var username = "";
+    var shouldPollForNewChatRooms = true;
     
     $("#username").bind('keyup', function (e) {
-        checkUserName($("#username"),false);
+        //checkUserName($("#username"), false);
     });
-    
+
     $('#login').on("submit",
     function (event) {
-    errorField.text("");
-    if (login($("#loginname").val(), $("#loginpassword").val()) == true) {
-        $('#loginDiv').hide();
-        return true;
-    } else {
-        showError("There is no User " + $("#loginname").val() + " registered");
-        return false;
-    }
-});
+        errorField.text("");
+        if (login($("#loginname").val(), $("#loginpassword").val()) == true) {
+            $('#loginDiv').hide();
+            return true;
+        } else {
+            showError("There is no User " + $("#loginname").val() + " registered");
+            return false;
+        }
+    });
+    
+    $("#createChannel").bind("click", function () {
+        if (isLoggedIn()) {
+            var newChannel = createChannel($("#newChannel"));
+            if (newChannel == '')
+                return;
+            hideError();
+            $.mobile.changePage("#chat?username=" + username + "&playerToken=" + playerToken + "&channel=" + newChannel, { transition: "slide" });
+        } else {
+            showError("You are not logged in");
+        }
+    });
 
-    function login(username, password) {
-        var playerTk;
+    $("#register").bind("click", function () {
+        $.mobile.changePage("#registerPage", { transition: "slide" });
+    });
+
+    function login(loginname, password) {
         $.ajax({
             type: "POST",
             url: serverUrl + "Login",
             dataType: "xml",
             async: false,
-            data: { playername: username, password: password },
+            data: { playername: loginname, password: password },
             success: function (xml) {
-                playerTk = $(xml).find("string").text();
+                playerToken = $(xml).find("string").text();
+                username = loginname;
             }
         });
-        if (playerTk == '') {
+        if (playerToken == '') {
             return false;
         }
         return true;
     }
-    $("#createChannel").bind("click", function () {
-        var username = checkUserName($("#username"),true);
-        if (username == '')
-            return false;
-        setPlayerToken();
-        if (playerToken == '')
-            showError("No player token - lol shouldnt happen, maybe server down");
-        var newChannel = createChannel($("#newChannel"));
-        if (newChannel == '')
-            return false;
-        hideError();
-        $.mobile.changePage("#chat?username=" + username + "&playerToken=" + playerToken + "&channel=" + newChannel, { transition: "slide" });
-    });
-    
-    $("#register").bind("click", function () {
-        $.mobile.changePage("#registerPage", { transition: "slide" });
-    });
+
 
     function reBind() {
-        $('#availableChannels li').bind("click", function() {
-            var username = checkUserName($("#username"), true);
-            if (username == '')
-                return false;
-            setPlayerToken();
-            if (playerToken == '')
-                showError("No player token - lol shouldnt happen, maybe server down");
-            hideError();
-            $.mobile.changePage("#chat?username=" + username + "&playerToken=" + playerToken + "&channel=" + $(this).attr("id"), { transition: "slide" });
-        });
-    }
-
-    function setPlayerToken() {
-        if (playerToken != '') return;
-        $.ajax({
-            type: "POST",
-            url: serverUrl + "Connect",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            async: false,
-            success: function (response) {
-                playerToken = response = response.d;
+        $('#availableChannels li').bind("click", function () {
+            if (isLoggedIn()) {
+                var newChannel = createChannel($("#newChannel"));
+                if (newChannel == '')
+                    return;
+                hideError();
+                $.mobile.changePage("#chat?username=" + username + "&playerToken=" + playerToken + "&channel=" + $(this).attr("id"), { transition: "slide" });
+            } else {
+                showError("You are not logged in");
             }
         });
     }
@@ -96,7 +84,7 @@ $(document).delegate("#lobby", "pagecreate", function () {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             async: false,
-            data: '{ "playerToken": "'+playerToken+'", "channelName": "'+newChannel.val()+'" }',
+            data: '{ "playerToken": "' + playerToken + '", "channelName": "' + newChannel.val() + '" }',
             success: function (response) {
                 newChannelName = response.d;
             }
@@ -106,7 +94,7 @@ $(document).delegate("#lobby", "pagecreate", function () {
         showError("Some Error happend on creating channel, try again");
         return '';
     }
-    
+
     function loadChannels() {
         if (shouldPollForNewChatRooms) {
             setTimeout(loadChannels, 5000);
@@ -125,7 +113,7 @@ $(document).delegate("#lobby", "pagecreate", function () {
                         var numberOfPlayers = response[i].Players.length;
                         channels.append('<li data-theme="c" id="' + chatId + '" data-transition="slide"><a href="#">' + chatName + ' (' + numberOfPlayers + ')</a></li>').listview("refresh");
                     }
-   
+
                     channels.trigger("create");
                     reBind();
                 }
@@ -133,30 +121,30 @@ $(document).delegate("#lobby", "pagecreate", function () {
         }
     }
 
-    function checkUserName(newUserName,checkIfIsEmpty) {
-        if (checkIfIsEmpty && newUserName.val().trim() == '') {
-            showError("You MUST enter a username, faggot!");
-            return '';
-        }
-        var isUnique = false;
-        $.ajax({
-            type: "POST",
-            url: serverUrl + "IsNameUnique",
-            data: '{ "name": "'+newUserName.val()+'" }',
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            async: false,
-            success: function (response) {
-                isUnique = response.d;
-            }
-        });
-        if (isUnique) {
-            hideError();
-            return newUserName.val();
-        }
-        showError("Your username is already taken idiot");
-        return '';
-    }
+    //function checkUserName(newUserName, checkIfIsEmpty) {
+    //    if (checkIfIsEmpty && newUserName.val().trim() == '') {
+    //        showError("You MUST enter a username, faggot!");
+    //        return '';
+    //    }
+    //    var isUnique = false;
+    //    $.ajax({
+    //        type: "POST",
+    //        url: serverUrl + "IsNameUnique",
+    //        data: '{ "name": "' + newUserName.val() + '" }',
+    //        contentType: "application/json; charset=utf-8",
+    //        dataType: "json",
+    //        async: false,
+    //        success: function (response) {
+    //            isUnique = response.d;
+    //        }
+    //    });
+    //    if (isUnique) {
+    //        hideError();
+    //        return newUserName.val();
+    //    }
+    //    showError("Your username is already taken idiot");
+    //    return '';
+    //}
 
     function isLoggedIn() {
         var loggedIn = false;
@@ -180,13 +168,13 @@ $(document).delegate("#lobby", "pagecreate", function () {
     function hideError() {
         errorField.css("display", "none");
     }
-    
+
     function pageBeforeHideFunction() {
         shouldPollForNewChatRooms = false;
     }
-    
+
     function pageBeforeShowFunction() {
-        if(isLoggedIn()) {
+        if (isLoggedIn()) {
             $('#loginDiv').hide();
         }
         shouldPollForNewChatRooms = true;
